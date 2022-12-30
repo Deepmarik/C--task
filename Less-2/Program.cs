6c9aed30238fc4f3608eda6c864dfe76acdba60f
Deepmarik <deepmarik@gmail.com>
2022-12-30 11:09:18 +0300

    created Less-2

--- Less-2/Program.cs
+++ Less-2/Program.cs
@@ -1,34 +1,17 @@
-﻿//Less-2
+﻿Console.WriteLine("Задача 8");
+int N = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+};
 /*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 456 -> 5
 782 -> 8
 918 -> 1*/
-int number = int.Parse(Console.ReadLine()!);
-int GetLastDigit(int number){
+Console.WriteLine("Задача 10");
+int Num = int.Parse(Console.ReadLine()!);
 
-    if (number < 100 || number > 999)
-    {
-        int LastDigit = number % 10;
-        Console.WriteLine(LastDigit);
-    }
-};
-
-GetLastDigit(number);
-/*{
-    Console.Write("Число не трехзначное");
-}
-else {
-    Console.WriteLine($"Последня цифра числа {number} - {GetLastDigit(number)}");
-}*/
-
-/*string str = a.ToString();
-int[] b = new int[str.Length];*/
-
-/*for( int i=0; i< str.Length; i++)
-
-{
-     b[i] = int.Parse(str[i].ToString);
-     Console.WriteLine(b);
-}
-*/
