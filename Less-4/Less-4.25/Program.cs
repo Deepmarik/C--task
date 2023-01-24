12550ac95b580b6adcab90a178b89e9bf9b80662
Deepmarik <deepmarik@gmail.com>
2023-01-24 18:39:22 +0300

    задача 25 решена

--- Less-4/Less-4.25/Program.cs
+++ Less-4/Less-4.25/Program.cs
@@ -1,3 +1,15 @@
 ﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
+int numA = int.Parse(Console.ReadLine()!);
+int numB = int.Parse(Console.ReadLine()!);
+int DegreeNumber(int a, int b)
+{
+    int deegree = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        deegree *= a;
+    }
+    return deegree;
+}
+Console.WriteLine (DegreeNumber(numA, numB));
