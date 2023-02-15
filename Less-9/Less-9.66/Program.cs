98a1f2b34b5c271bc60014aa0f455c71cda1b7c9
Deepmarik <deepmarik@gmail.com>
2023-02-15 17:31:27 +0300

    задача 66 решена

--- Less-9/Less-9.66/Program.cs
+++ Less-9/Less-9.66/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+int m = int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
+int sum = 0;
+for (int i = m; i <= n; i++)
+{
+    sum += i;
+};
+Console.Write($"Сумма чисел : {sum}");
