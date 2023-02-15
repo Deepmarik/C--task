02356e6d1bc8a9cf81418bbd0c529bc592009087
Deepmarik <deepmarik@gmail.com>
2023-02-15 17:13:35 +0300

    создана ветка less-9

--- Less-9/Less-9.66/obj/Debug/net6.0/Less-9.66.AssemblyInfo.cs
+++ Less-9/Less-9.66/obj/Debug/net6.0/Less-9.66.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Less-5.36")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Less-9.66")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Less-5.36")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Less-5.36")]
+[assembly: System.Reflection.AssemblyProductAttribute("Less-9.66")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Less-9.66")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
