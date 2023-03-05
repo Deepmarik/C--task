ddbbe50755503967081ebd5a7102faf160975394
Deepmarik <deepmarik@gmail.com>
2023-03-05 18:06:33 +0300

    задача 66 решена

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -2,5 +2,18 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30*/
 
-
+int SumNumbers(int m, int n)
+{
+    if (m == 0) return (n * (n + 1)) / 2;            // Если m равно нулю
+    else if (n == 0) return (m * (m + 1)) / 2;       // Если n равно нулю
+    else if (m == n) return m;                       // Если m=n
+    else if (m < n) return n + SumNumbers(m, n - 1); // Если m<n
+    else return n + SumNumbers(m, n + 1);            // Если m>n
+}
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма = {SumNumbers(m, n)}");
+//Console.ReadLine();
 
